0d4c4d0605c3c8caeb8d6d9759d6c2f9cd7689f7
xiaoyang <1483863489@qq.com>
2018-08-30 10:56:38 +0800

    添加注册中心

--- zookeeper-server/src/main/java/com/xiaoyang/zookeeperserver/ZookeeperServerApplication.java
+++ zookeeper-server/src/main/java/com/xiaoyang/zookeeperserver/ZookeeperServerApplication.java
@@ -1,8 +1,5 @@
 package com.xiaoyang.zookeeperserver;
 
-import org.springframework.boot.SpringApplication;
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,12 +7,8 @@ public class ZookeeperServerApplication {
 
 
 	public static void main(String[] args) {
-		
 		// start embedded zookeeper server
 		new EmbeddedZooKeeper(2181, false).start();
-
-		
-		SpringApplication.run(ZookeeperServerApplication.class, args);
 	}
 	
 }
