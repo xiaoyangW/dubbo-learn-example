162fc3fe4b874f30d071462e6555452e23832b57
xiaoyang <1483863489@qq.com>
2018-08-31 10:12:35 +0800

    添加dubbo-service

--- dubbo-service/src/main/java/com/xiaoyang/DubboServiceImpl.java
+++ dubbo-service/src/main/java/com/xiaoyang/DubboServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import com.alibaba.dubbo.config.annotation.Service;
 
 @Service(version = "1.0.0")
-public class HelloServiceImpl implements HelloService {
+public class DubboServiceImpl implements DubboService {
 
 	@Override
     public String sayHello(String name) {
