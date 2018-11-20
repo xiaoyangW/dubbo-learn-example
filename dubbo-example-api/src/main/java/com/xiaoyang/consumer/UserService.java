a4ef6f5aca4d62397e3738290fa11d6b6f2c8298
xiaoyang <1483863489@qq.com>
2018-11-20 20:23:13 +0800

    添加api项目

---
+++ dubbo-example-api/src/main/java/com/xiaoyang/consumer/UserService.java
@@ -0,0 +1,14 @@
+package com.xiaoyang.consumer;
+
+/**
+ * @author WXY
+ * @date 2018/11/19 21:01
+ */
+
+public interface UserService {
+
+    String getUser(String name);
+
+    Integer addUser();
+
+}
---
+++ dubbo-example-api/src/main/java/com/xiaoyang/pojo/User.java
@@ -0,0 +1,18 @@
+package com.xiaoyang.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author WXY
+ * @date 2018/11/19 21:03
+ */
+@Setter
+@Getter
+@ToString
+public class User {
+
+    private String name;
+
+}
