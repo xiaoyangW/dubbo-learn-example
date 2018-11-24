57d2e49548fc37f131073fd349fc21ca16e9a329
xiaoyang <1483863489@qq.com>
2018-11-24 22:02:38 +0800

    对象序列化

--- dubbo-example-api/src/main/java/com/xiaoyang/pojo/User.java
+++ dubbo-example-api/src/main/java/com/xiaoyang/pojo/User.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 /**
  * @author WXY
  * @date 2018/11/19 21:03
@@ -11,10 +13,17 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
-public class User {
+public class User implements Serializable {
 
     private Integer id;
 
     private String name;
 
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+    }
 }
